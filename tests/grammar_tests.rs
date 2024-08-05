ee80fc9db578e27ad3e6627f648ff257dcb93c4b
m <m@mcarr.one>
2024-08-05 14:23:03 -0700

    finish test for grammar predictiveness

--- tests/grammar_tests.rs
+++ tests/grammar_tests.rs
@@ -100,6 +100,17 @@ impl<'a> Grammar<'a> {
             });
         }
 
+        if let Some(x) = rules.get(0) {
+            let nt = x.nonterminal;
+            rules
+                .iter_mut()
+                .filter(|r| r.nonterminal == nt)
+                .for_each(|x| {
+                    x.productions
+                        .push(ProductionItem::Terminal(Terminal::Named("<end of file>")));
+                })
+        }
+
         Ok(Self {
             nonterminals,
             terminals,
@@ -107,51 +118,199 @@ impl<'a> Grammar<'a> {
         })
     }
 
-    /// Returns the first conflict `(item, rule)` where `rule` has non-disjoint
-    /// predictive set (first + follow set) for its nonterminal because of `item`
-    fn non_predictive(&self) -> Option<(&'_ ProductionItem<'a>, &'_ Rule<'a>)> {
+    /// Returns the predict sets (for each rule, FIRST union FOLLOW) for each rule.
+    ///
+    /// TODO: return left- and right-recursion dependency cycle sets instead of panicking/warning
+    ///
+    /// # Panics
+    ///
+    /// Panics if the grammar is **left-recursive**.
+    fn predict_sets(&self) -> Vec<BTreeSet<Terminal<'a>>> {
         let mut first_sets: BTreeMap<_, _> = self
             .nonterminals
             .iter()
             .map(|&nt| (nt, BTreeSet::new()))
             .collect();
 
+        let mut predict_sets = vec![BTreeSet::new(); self.rules.len()];
+
+        let mut follow_sets: BTreeMap<_, _> = self
+            .nonterminals
+            .iter()
+            .map(|&nt| (nt, BTreeSet::<Terminal>::new()))
+            .collect();
+
         let mut expand = self.nonterminals.clone();
-        let mut follow = BTreeSet::new();
 
         while !expand.is_empty() {
             let mut next = BTreeSet::new();
             for &nt in &expand {
-                for rule in self.rules.iter().filter(|x| x.nonterminal == nt) {
-                    if let Some(item) = rule.productions.first() {
+                for (i, rule) in self
+                    .rules
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, x)| x.nonterminal == nt)
+                {
+                    let mut follow = true;
+                    for item in &rule.productions {
+                        if !follow {
+                            break;
+                        }
+
                         match item {
                             ProductionItem::Terminal(t) => {
-                                first_sets.get_mut(&nt).unwrap().insert(*t);
+                                first_sets.get_mut(&nt).unwrap().insert(Some(*t));
+                                predict_sets[i].insert(Some(*t));
+                                follow = false;
                             }
-                            ProductionItem::Nonterminal(other) => {
-                                let items = first_sets[other].clone();
-                                first_sets.get_mut(&nt).unwrap().extend(items.into_iter());
-                                if expand.contains(&other) {
+                            ProductionItem::Nonterminal(next_nt) => {
+                                let mut next_first = first_sets[next_nt].clone();
+                                follow = next_first.remove(&None);
+
+                                predict_sets[i].extend(next_first.iter().copied());
+                                first_sets.get_mut(&nt).unwrap().extend(next_first);
+
+                                if expand.contains(next_nt) {
                                     next.insert(nt);
                                 }
                             }
                         }
-                    } else {
-                        follow.insert(nt);
+                    }
+
+                    if follow {
+                        first_sets.get_mut(&nt).unwrap().insert(None);
+                        predict_sets[i].insert(None);
                     }
                 }
             }
 
             if expand == next {
-                panic!("Grammar is left recursive: dependencies are in {next:?}");
+                panic!("Grammar is left recursive: dependency cycle: {next:?}\nfirst sets: {first_sets:?}");
             }
 
             expand = next;
         }
 
-        panic!("{first_sets:?}");
+        expand = first_sets
+            .iter()
+            .filter_map(|(&nt, set)| set.contains(&None).then_some(nt))
+            .collect();
 
-        None
+        let mut changed = true;
+        while changed {
+            let mut next = BTreeSet::new();
+            changed = false;
+
+            for &nt in &expand {
+                // In a predictive grammar, this would be at most one, but we don't know if the grammar
+                // is predictive yet.
+                let follow_rules: Vec<_> = self
+                    .rules
+                    .iter()
+                    .enumerate()
+                    .filter(|(i, r)| r.nonterminal == nt && predict_sets[*i].contains(&None))
+                    .collect();
+
+                for rule in &self.rules {
+                    for (i, _) in rule
+                        .productions
+                        .iter()
+                        .enumerate()
+                        .filter(|(_, &x)| x == ProductionItem::Nonterminal(nt))
+                    {
+                        let mut follow = true;
+                        for item in &rule.productions[i + 1..] {
+                            if !follow {
+                                break;
+                            }
+                            match item {
+                                ProductionItem::Terminal(t) => {
+                                    follow_sets.get_mut(&nt).unwrap().insert(*t);
+                                    for (j, _) in &follow_rules {
+                                        predict_sets[*j].insert(Some(*t));
+                                    }
+                                    follow = false;
+                                }
+                                ProductionItem::Nonterminal(other) => {
+                                    let set = &first_sets[other];
+                                    follow = set.contains(&None);
+                                    let mut set: BTreeSet<_> =
+                                        set.iter().filter_map(|x| *x).collect();
+                                    if follow {
+                                        if expand.contains(other) {
+                                            next.insert(nt);
+                                        }
+
+                                        set.extend(follow_sets[other].iter().copied());
+                                    }
+
+                                    for (j, _) in &follow_rules {
+                                        predict_sets[*j].extend(set.iter().map(|&x| Some(x)));
+                                    }
+
+                                    changed |= !follow_sets[&nt].is_superset(&set);
+                                    follow_sets.get_mut(&nt).unwrap().extend(set);
+                                }
+                            };
+                        }
+
+                        if follow {
+                            let set = follow_sets[&rule.nonterminal].clone();
+
+                            for (j, _) in &follow_rules {
+                                predict_sets[*j].extend(set.iter().map(|&x| Some(x)));
+                            }
+
+                            changed |= !follow_sets[&nt].is_superset(&set);
+                            follow_sets.get_mut(&nt).unwrap().extend(set);
+
+                            if expand.contains(&rule.nonterminal) {
+                                next.insert(nt);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if next == expand {
+                eprintln!("Right-recursion detected:  dependency cycle: {next:?}.\nLooping until no change...");
+            }
+
+            expand = next;
+        }
+
+        predict_sets
+            .into_iter()
+            .map(|x| x.into_iter().filter_map(|x| x).collect())
+            .collect()
+    }
+
+    fn non_predictive(&self) -> Vec<(usize, BTreeSet<Terminal<'a>>)> {
+        let predict_sets = self.predict_sets();
+        for (i, l) in predict_sets.iter().enumerate() {
+            println!("{i}: {l:?}");
+        }
+        self.nonterminals
+            .iter()
+            .flat_map(|&nt| {
+                self.rules
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, x)| x.nonterminal == nt)
+                    .fold(
+                        (BTreeSet::new(), Vec::new()),
+                        |(set, mut errors), (i, _)| {
+                            let intersection: BTreeSet<_> =
+                                set.intersection(&predict_sets[i]).copied().collect();
+                            if !intersection.is_empty() {
+                                errors.push((i, intersection));
+                            }
+                            (set.union(&predict_sets[i]).copied().collect(), errors)
+                        },
+                    )
+                    .1
+            })
+            .collect()
     }
 }
 
@@ -162,7 +321,24 @@ fn get_grammar() -> Grammar<'static> {
 #[test]
 fn is_predictive() {
     let grammar = get_grammar();
-    assert_eq!(grammar.non_predictive(), None, "grammar is not predictive");
+    let non_predictive = grammar.non_predictive();
+
+    if !non_predictive.is_empty() {
+        eprintln!("--- rules ---");
+        for (i, rule) in grammar.rules.iter().enumerate() {
+            eprint!("{i}: {} ->", rule.nonterminal.0);
+            for p in &rule.productions {
+                match p {
+                    ProductionItem::Terminal(Terminal::Literal(s))
+                    | ProductionItem::Terminal(Terminal::Named(s))
+                    | ProductionItem::Nonterminal(Nonterminal(s)) => eprint!(" {s}"),
+                }
+            }
+            eprintln!();
+        }
+
+        panic!("Grammar is non-predictive: {non_predictive:?}");
+    }
 }
 
 #[test]
@@ -178,5 +354,5 @@ fn test_grammar_recursive() {
     )
     .unwrap();
 
-    grammar.non_predictive();
+    let _ = grammar.predict_sets();
 }
