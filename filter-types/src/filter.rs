dd0865a38d0e746996664d2c2bfcb1167620a741
m <m@mcarr.one>
2024-09-12 20:53:10 -0700

    doc fixes

--- filter-types/src/filter.rs
+++ filter-types/src/filter.rs
@@ -7,9 +7,13 @@ use super::{
 
 pub use scrapelect_filter_proc_macro::{filter_fn, Args};
 
-/// Typed arguments for a [`Filter`].  It can be implemented manually, but you
-/// can also use the derive macro [`Args`](scrapelect_filter_proc_macro::Args)
-/// if all the fields in the struct implement [`TryFromValue<Element>`](TryFromValue).
+/// Typed arguments for a [`Filter`].
+///
+/// If all the fields in the struct implement [`TryFromValue<Element`], it is easier
+/// to use the derive macro [`Args`], but this can also be implemented manually if
+/// you need to express more custom deserializing logic.
+///
+/// [`Args`](scrapelect_filter_proc_macro::Args)
 pub trait Args<'doc>: Sized {
     /// Try to deserialize the typed arguments from the given `args`.
     ///
@@ -59,18 +63,17 @@ pub trait Filter {
     ) -> Result<PValue<'doc>>;
 }
 
-#[allow(clippy::doc_lazy_continuation)]
 /// An object-safe version of [`Filter`]. All `F: Filter` implement this trait,
 /// so prefer implementing `Filter` unless you must:
 ///
 /// 1. Deserialize the input [`PValue`] in a custom way (not using [`TryFromValue`])
 /// 2. Use custom arg-deserializing logic (but often you will be able to implement
-///   [`Args`] manually instead, and still get the typed guarantees of [`Filter`])
+///    [`Args`] manually instead, and still get the typed guarantees of [`Filter`])
 /// 3. Use the `&self` reference.  This *can* be used to store state with interior
-///   mutability (though note that in `scrapelect`, filters must be `Send + Sync`) to
-///   register, but it is often not the best idea to have filter state because filters
-///   can be called from anywhere in the program, and you will have to reason out the
-///   soundness of having the state.
+///    mutability (though note that in `scrapelect`, filters must be `Send + Sync`) to
+///    register, but it is often not the best idea to have filter state because filters
+///    can be called from anywhere in the program, and you will have to reason out the
+///    soundness of having the state.
 pub trait FilterDyn {
     /// Call this filter with the given `value`, `args`, and `ctx`.
     ///
